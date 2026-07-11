d88b940c9d15468628160d25faa82282b2a115a4
Kushwant Singh Rathore <kushwantsinghrathore0@gmail.com>
2026-07-11 19:05:01 +0530

    Adding main.py in GenAI-Assignment5-KushwantSinghRathore

---
+++ 06-GenAI/Python_Fundamentals/GenAI-Assignment5-KushwantSinghRathore/main.py
@@ -0,0 +1,10 @@
+import math_utils as mu
+from math_utils import square
+
+a = int(input("Enter value 1: "))
+b = int(input("Enter value 2: "))
+
+n = int(input("Enter value for square: "))
+print(f"Addition is: {mu.add(a,b)}")
+print(f"Subtraction is: {mu.subtract(a,b)}")
+print(f"Square is: {square(n)}")
