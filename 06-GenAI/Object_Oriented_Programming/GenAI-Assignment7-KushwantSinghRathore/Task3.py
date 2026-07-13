8780ed2b130b3e5beb5d039e5db785db1f2025a1
Kushwant Singh Rathore <kushwantsinghrathore0@gmail.com>
2026-07-13 18:33:54 +0530

    Adding Task3.py in GenAI-Assignment7-KushwantSinghRathore (Object-Oriented-Programming)

---
+++ 06-GenAI/Object_Oriented_Programming/GenAI-Assignment7-KushwantSinghRathore/Task3.py
@@ -0,0 +1,19 @@
+class Product:
+    def __init__(self, name, price, category):
+        self.name = name
+        self.price = price
+        self.category = category
+    
+    def get_info(self):
+        return f"Product: {self.name}, Price: ${self.price}, Category: {self.category}"
+
+class ElectronicProducts(Product):
+    def __init__(self,name,price,category,warranty_years):
+        super().__init__(name,price,category)
+        self.warranty_years = warranty_years
+
+    def get_info(self):
+        return f"{super().get_info()}, Warranty: {self.warranty_years} years"
+
+laptop = ElectronicProducts("Dell XPS", 1200, "Electronics", 3)
+print(laptop.get_info())
